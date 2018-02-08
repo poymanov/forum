7c8dbbb9eb9987cf9b1953572cede5202a1e5b73
Nikolay Poymanov <n.poymanov@gmail.com>
2018-02-09 01:22:40 +0300

    Add threads routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/threads', 'ThreadsController@index')->name('threads');
+Route::get('/threads/{thread}', 'ThreadsController@show')->name('threads.show');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
